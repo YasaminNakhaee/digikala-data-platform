b48aaf3b1cfb62b0f9f12b89189b2421ab812e8b
YasaminNakhaee <0nakhaeeyasamin0@gmail.com>
2026-08-11 11:55:15 -0700

    feat : complete robust data import pipeline with cleaning and logging

--- src/scripts/import_data.py
+++ src/scripts/import_data.py
@@ -1,25 +1,45 @@
 import pandas as pd
+import logging
 from src.db.database import engine
+
+logging.basicConfig(
+    filename="logs/import_errors.log" , 
+    level=logging.ERROR , 
+    format="%(asctime)s - %(levelname)s - %(massage)s"
+)
 
 def run_import_pipeline():
     file_path = "data/digikala-comments.csv"
-    chunk_size = 10000
+    chunk_size = 50000
 
     print("start")
 
     try:
-        for chunk in pd.read_csv(file_path, chunksize=chunk_size):
-            print(f"processing chunk with {len(chunk)} rows")
+        total_rows = 0
 
-            chunk.to_sql("comments", engine , if_exists = "append", index = False)
+        for i, chunk in enumerate ( pd.read_csv(file_path, chunksize=chunk_size)): 
 
-            print("chunk inserted successfully")
+            try: 
 
-            break
+                chunk = chunk.drop_duplicates()
 
+                if "body" in chunk.columns:
+                    chunk = chunk.dropna(subset={"body"})
+
+                chunk.to_sql("comments", engine , if_exists = "append", index = False)
+                total_rows += len(chunk)
+                print(f"chunk {i+1} inserted . total_rows : {total_rows} ")
+
+            except Exception as e:
+            
+                    logging.error(f"error in chunk {i+1} : {e}")
+                    print("fatalerror check : logs/import_errors.log ")
+            
+        print(f"finish inserted : {total_rows}")
     except Exception as e:
 
-        print(f"error: {e}")
+        print("fatalerror check : logs/import_errors.log ")
+        logging.critical(f"fatalerror durig inport : {e}")
 
 if __name__ == "__main__" : 
     run_import_pipeline()
